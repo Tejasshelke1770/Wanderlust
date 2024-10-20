4bb19aa1507fad8a27451e267bcdfe9742f89393
Tejasshelke1770 <shelket17@gmail.com>
2024-10-20 22:20:38 +0530

    changes in connection.js

--- utils/connection.js
+++ utils/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const dbUrl = process.env.ATLASDB_URL;
+// const dbUrl = process.env.ATLASDB_URL;
 
 module.exports  = async function connection() {
-    await mongoose.connect(dbUrl);
+    await mongoose.connect('mongodb+srv://tejasshelke:JQUMaU2OvzL1xcGu@cluster0.pew6f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
   }
